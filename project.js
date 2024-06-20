b92c22a70f591bd410457c74fb008d2559c32385
Kariuki11 <irungukenneth433@gmail.com>
2024-06-20 05:03:20 +0300

    New commita

--- project.js
+++ project.js
@@ -9,5 +9,9 @@ const prompt = require("prompt-sync")();
 
 const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || ) 
 };
 
+deposit();
