140aeee48794bde6b63fca9a22eee8b1eeec608c
Kariuki11 <irungukenneth433@gmail.com>
2024-06-20 05:21:07 +0300

    New commits

--- project.js
+++ project.js
@@ -21,4 +21,17 @@ const deposit = () => {
     }
 };
 
+const getNumberofLines = () => {
+    while(true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+            const numberOfLines = parseFloat(lines);
+    
+            if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+                console.log("Invalid number of lines. Try again!.");
+            } else {
+                return numberDepositAmount;
+            }
+        }
+}
+
 const depositAmount = deposit();
