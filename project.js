29a61c5b32094f488b0e92541c0d4abb240b6003
Kariuki11 <irungukenneth433@gmail.com>
2024-06-20 06:21:13 +0300

    New changes

--- project.js
+++ project.js
@@ -76,7 +76,8 @@ const Spin = () => {
     for (let i = 0; i < COLS; i++) {
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
-            
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
         }
 }
 Spin();
