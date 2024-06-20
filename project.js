5a85ac1543582543eaa342a787e71799647c9a8b
Kariuki11 <irungukenneth433@gmail.com>
2024-06-20 05:08:19 +0300

    New commits

--- project.js
+++ project.js
@@ -8,12 +8,14 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
+    while(true) {
     const depositAmount = prompt("Enter a deposit amount: ");
         const numberDepositAmount = parseFloat(depositAmount);
 
         if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
             console.log("Invalid deposit amount. Try again!.");
         } 
+    }
 };
 
 deposit();
