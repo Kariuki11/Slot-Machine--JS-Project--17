56e50470b10078e527314625305862fc12557d45
Kariuki11 <irungukenneth433@gmail.com>
2024-06-22 10:40:26 +0300

    New changes

--- project.js
+++ project.js
@@ -117,7 +117,12 @@ const getWinnings = (rows, bet, lines) => {
     let winnings = 0;
 
     for (let row = 0; row < lines; row++) {
-        
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+        }
     }
 }
 
